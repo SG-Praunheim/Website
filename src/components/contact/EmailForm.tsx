04074147d92e811aebecdc93f1d7ad6f886e954e
Niklas Deeg <the_niklas@hotmail.de>
2024-09-01 22:44:23 +0200

    Kontakt Form funktioniert jetzt. nachricht Feld noch bugt

--- src/components/contact/EmailForm.tsx
+++ src/components/contact/EmailForm.tsx
@@ -2,10 +2,10 @@
 
 import React, { useEffect } from "react";
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import Stack from '@mui/material/Stack';
-import { FC } from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export type FormData = {
     name: string;
@@ -41,11 +41,84 @@ export default function EmailForm() {
             <Typography align='center' variant="h4" gutterBottom>Kontaktiere Uns Jetzt</Typography>
 
             <form onSubmit={handleSubmit}>
+              <Stack direction="column" spacing={2} useFlexGap>
+
+              <InputLabel sx={{alignContent: 'center'}}>Name</InputLabel>
+                <TextField
+                required
+                  id="email-name"
+                  hiddenLabel
+                  size="small"
+                  variant="outlined"
+                  fullWidth
+                  name="name"
+                  aria-label="Deine Name"
+                  placeholder="Deine name"
+                  slotProps={{
+                    htmlInput: {
+                      autoComplete: 'off',
+                      'aria-label': 'Deine Name',
+                    },
+                  }}
+                  sx={{ width: '100%' }}
+                />
+
+              <InputLabel sx={{alignContent: 'center'}}>Email</InputLabel>
+                <TextField
+                required
+                  id="email-newsletter"
+                  hiddenLabel
+                  size="small"
+                  variant="outlined"
+                  fullWidth
+                  name="email"
+                  aria-label="Deine E-Mail Adresse"
+                  placeholder="Deine E-Mail Adresse"
+                  slotProps={{
+                    htmlInput: {
+                      autoComplete: 'off',
+                      'aria-label': 'Deine E-Mail Adresse',
+                    },
+                  }}
+                  sx={{ width: '100%' }}
+                />
+
+<InputLabel sx={{alignContent: 'center'}}>Nachricht</InputLabel>
+                <TextField
+                 multiline
+                 required                  
+                  variant="outlined"
+                  fullWidth
+                  name="nachricht"
+                  aria-label="Deine Nachricht an uns"
+                  placeholder="Deine Nachricht an uns"
+                  slotProps={{
+                    htmlInput: {
+                      autoComplete: 'off',
+                      'aria-label': 'Deine Nachricht an uns',
+                    },
+                  }}
+                  sx={{ width: '100%' }}
+                />
+
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                  type="submit"
+                  sx={{ flexShrink: 0 }}
+                >
+                  Nachricht abschicken
+                </Button>
+              </Stack>
+              </form>
+
+            {/* <form onSubmit={handleSubmit}>
                 <input type="text" name="name" />
                 <input type="email" name="email" />
                 <textarea name="message"></textarea>
                 <button type="submit">Submit Form</button>
-            </form>
+            </form> */}
 
         </React.Fragment>
 
