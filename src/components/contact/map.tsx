d50638a07a4782cc9b68dea21058c7a36c22d05e
Niklas Deeg <the_niklas@hotmail.de>
2024-09-01 17:23:21 +0200

    Neue Location 2

--- src/components/contact/map.tsx
+++ src/components/contact/map.tsx
@@ -21,8 +21,8 @@ export default function Map() {
             const {Marker} = await loader.importLibrary('marker') as google.maps.MarkerLibrary;
 
             const position = {
-                lat: 50.150071,
-                lng: 8.620723,
+                lat: 50.149970,
+                lng: 8.621500,
             };
 
             // map options
