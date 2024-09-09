dc49e1e6e168b19911e2f508a56e4068c85d8558
Niklas Deeg <the_niklas@hotmail.de>
2024-09-09 10:34:57 +0200

    Meta Data von alter Website übernommen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "SG Praunheim",
   description: "Website vom Kegelsportverein SG Praunheim",
-  keywords:"Kegeln, Praunheim, SG Praunheim, Alt-Praunheim, Kegelclub, Kegeln Frankfurt, Kegeln Praunheim, Kegelbahn, Gut Holz, Alle Neun, KV Praunheim, GK Praunheim ",
+  keywords: "Kegeln, Praunheim, SG Praunheim, Alt-Praunheim, Kegelclub, Kegeln Frankfurt, Kegeln Praunheim, Kegelbahn, Gut Holz, Alle Neun, KV Praunheim, GK Praunheim ",
 };
 
 export default function RootLayout({
@@ -17,7 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head><meta name="google-site-verification" content="Qn3aResZKUbIX6F4n-SZAfoPcK-AEaINi5mMCxSzB20" /></head>      
+      <head>
+        <meta name="google-site-verification" content="Qn3aResZKUbIX6F4n-SZAfoPcK-AEaINi5mMCxSzB20" />
+        <meta property="business:hours:day" content="TUESDAY" />
+        <meta property="business:hours:start" content="17:00" />
+        <meta property="business:hours:end" content="22:00" />
+        <meta property="business:contact_data:country_name" content="Deutschland" />
+        <meta property="business:contact_data:street_address" content="Alt-Praunheim 44" />
+        <meta property="business:contact_data:locality" content="Frankfurt am Main" />
+        {/* <meta property="business:contact_data:region" content="300"/> */}
+        {/* <meta property="business:contact_data:email" content="thomas.franz@utas.utc.com"/> */}
+        <meta property="business:contact_data:postal_code" content="60488" />
+        {/* <meta property="business:contact_data:phone_number" content=" +49 179 7390897"/> */}
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
