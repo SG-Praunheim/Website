8e0ba48ba94c22da390de65701c21a2c95d896a3
Niklas Deeg <the_niklas@hotmail.de>
2024-09-09 13:12:20 +0200

    FAQ Anpassung

--- src/components/faq/Faq.tsx
+++ src/components/faq/Faq.tsx
@@ -20,7 +20,7 @@ import Collapse from '@mui/material/Collapse';
 
 const faqText0: string = "Na klar! Komm einfach Dienstags ab 18 Uhr vorbei und bring saubere Turnschuhe mit. Es ist bestimmt jemand von uns da. Oder mach vorher einen Termin mit uns aus."
 const faqText1: string = "Eine Altergrenze gibt es nicht. Bei uns kann man die komplette Jugend durchlaufen und auch mit 80 noch Spaß am Kegeln haben."
-const faqText2: string = "Die Miete einer Bahn kostet momentan 9€ die Stunde (kein Vergleich zu einer Bowlingbahn!). Kontaktiere uns oder unseren Wirt Tasso wenn du eine Bahn mieten möchtest."
+const faqText2: string = "Wir können bis zu 4 Bahnen vermieten, perfekt für Geburtstagsfeiern, Teamevents oder zum Schoppekegeln. Die Miete einer Bahn kostet momentan 9€ die Stunde (kein Vergleich zu einer Bowlingbahn!). Kontaktiere uns oder unseren Wirt Tasso, falls du eine Bahn mieten möchtest."
 
 const StyledTypography = styled(Typography)({
 
